e8f2931d8fbb68f6a85cd7526a451c85a393c639
Andrew Lloyd Cartwright <andrewlloydcartwright+atlc@gmail.com>
2019-07-19 09:06:44 -0500

    Console function to convert either a JSON request body or the first record of a JSON response to a Markdown table with their basic schema

---
+++ convertJSONtoMarkdown.js
@@ -0,0 +1,15 @@
+function convertToMarkdownTable(JSONbody) {
+    if (JSONbody.Request !== undefined) {
+        var records = JSONbody.Request[Object.keys(JSONbody.Request)[0]].records[0];
+    } else if (JSONbody.Response !== undefined) {
+        var records = JSONbody.Response[Object.keys(JSONbody.Response)[0]].records[0];
+    } else {
+        return 'You have submitted an invalid JSON body';
+    }
+
+    let markdownStr = `| Field | Required | Description/Type |\n| --- | --- | --- |\n`;
+    for (i=0; i < Object.keys(records).length; i++) {
+        markdownStr += `| ${Object.keys(records)[i]} | ?? | ${typeof Object.values(records)[i]} |\n`;
+    }
+    return markdownStr;
+}
